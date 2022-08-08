a0b2cbee5da7b0669971084fba30688abd73eb3b
Jonas Jucker <joni.jucker@yahoo.de>
2022-08-08 18:32:58 +0200

    SW and LW models implemented

--- ecrad/multiple_fields_model.f90
+++ ecrad/multiple_fields_model.f90
@@ -9,159 +9,195 @@
 !
 
 program my_program
-use inferof
-use iso_c_binding, only : c_float, c_null_char
-implicit none
+  use inferof
+  use iso_c_binding, only : c_float, c_null_char
+  implicit none
 
-integer, parameter :: n_inference_reps = 1
-integer :: i
+  ! Command line arguments
+  character(1024) :: model_path_sw,model_path_lw
+  character(1024) :: model_type
+  character(1024) :: yaml_config
 
-! Command line arguments
-character(1024) :: model_path
-character(1024) :: model_type
-character(1024) :: yaml_config
+  ! model of infero model
+  type(infero_model) :: model_sw,model_lw
 
-! model of infero model
-type(infero_model) :: model
+  ! input and output tensors
+  real(c_float) :: input_3d(1,60,4,6)
+  real(c_float) :: input_2d(1,4,8)
+  real(c_float) :: pred_swflx(1,60,2), pred_lwflx(1,60,2)
+  type(infero_tensor_set) :: iset
+  type(infero_tensor_set) :: oset_lw, oset_sw
 
-! input and output tensors
-real(c_float) :: input_3d(1,60,4,6)
-real(c_float) :: input_2d(1,4,8)
-real(c_float) :: pred_swflx_dn(1,60,2) = 0
-type(infero_tensor_set) :: iset
-type(infero_tensor_set) :: oset
+  ! netcdf
+  character(1024) :: nc_name,varname,icon_grid
+  character(50) :: dim_name(6),grid_dim_name(14)
+  integer :: dim_len(6),grid_dim_len(14)
 
-! netcdf
-character(1024) :: nc_name,varname,icon_grid
-character(50) :: dim_name(6),grid_dim_name(14)
-integer :: dim_len(6),grid_dim_len(14)
+  ! indices from notebook
+  integer :: step, idx,n_step
 
-! indices from notebook
-integer :: x_cell_index=1234,x_time_index=5
+  real(c_float), allocatable :: from_netcdf_3d(:,:,:,:), from_netcdf_2d(:,:,:), neighbor_cell_index(:,:)
+  real(c_float), allocatable :: swflx(:,:,:,:), lwflx(:,:,:,:)
 
-real(c_float), allocatable :: from_netcdf_3d(:,:,:,:), from_netcdf_2d(:,:,:),neighbor_cell_index(:,:)
+  ! Get CL arguments
+  CALL get_command_argument(1, model_path_lw)
+  CALL get_command_argument(2, model_path_sw)
+  CALL get_command_argument(3, model_type)
 
-nc_name='/scratch/snx3000/juckerj/WG_1_tasks/ecrad/regress-radiation/&
-      &online-datasets/datasets/APE_JABW_R02B05_G_ECRAD_icongrid_atm_3d_DOM01_ml_0024_lonlat.nc'
+  ! 0) init infero
+  call infero_check(infero_initialise())
 
-icon_grid='/scratch/snx3000/juckerj/WG_1_tasks/ecrad/grids/icon_grid_0008_R02B05_G.nc'
+  ! YAML config string -> LW
+  yaml_config = "---"//NEW_LINE('A') &
+    //"  path: "//TRIM(model_path_lw)//NEW_LINE('A') &
+    //"  type: "//TRIM(model_type)//c_null_char
 
-call icon_grid_dims(icon_grid,grid_dim_name,grid_dim_len)
-ALLOCATE(neighbor_cell_index(grid_dim_len(1),grid_dim_len(5)))
+  ! get a infero model
+  call infero_check(model_lw%initialise_from_yaml_string(yaml_config))
 
-varname="neighbor_cell_index"
-call readgrid_1d(icon_grid,varname,neighbor_cell_index(:,:),grid_dim_len(1),grid_dim_len(5))
+  ! YAML config string -> SW
+  yaml_config = "---"//NEW_LINE('A') &
+    //"  path: "//TRIM(model_path_sw)//NEW_LINE('A') &
+    //"  type: "//TRIM(model_type)//c_null_char
 
-call griddims(nc_name,dim_name,dim_len)
+  ! get a infero model
+  call infero_check(model_sw%initialise_from_yaml_string(yaml_config))
 
-ALLOCATE(from_netcdf_3d(dim_len(1),dim_len(3),dim_len(6),6))
-ALLOCATE(from_netcdf_2d(dim_len(1),dim_len(6),8))
+  ! init tensor sets
+  call infero_check(iset%initialise())
+  call infero_check(oset_lw%initialise())
+  call infero_check(oset_sw%initialise())
 
-! 2D FIELDS
-varname="pres_sfc"
-call readgrid_1d(nc_name,varname,from_netcdf_2d(:,:,1),dim_len(1),dim_len(6))
+  n_step = 96
+  idx=1235
 
-varname="cosmu0"
-call readgrid_1d(nc_name,varname,from_netcdf_2d(:,:,2),dim_len(1),dim_len(6))
+  ALLOCATE(swflx(1,60,2,n_step))
+  ALLOCATE(lwflx(1,60,2,n_step))
 
-varname="qv_s"
-call readgrid_1d(nc_name,varname,from_netcdf_2d(:,:,3),dim_len(1),dim_len(6))
+  
+  ! strange behaviour with init to 0 -> all values stay 0 even after infer
+  !pred_swflx(1,:,:) = 0
+  !pred_lwflx(1,:,:) = 0
 
-varname="albvisdir"
-call readgrid_1d(nc_name,varname,from_netcdf_2d(:,:,4),dim_len(1),dim_len(6))
+  nc_name='/scratch/snx3000/juckerj/WG_1_tasks/ecrad/regress-radiation/&
+        &online-datasets/datasets/APE_JABW_R02B05_G_ECRAD_icongrid_atm_3d_DOM01_ml_0024_lonlat.nc'
 
-varname="albnirdir"
-call readgrid_1d(nc_name,varname,from_netcdf_2d(:,:,5),dim_len(1),dim_len(6))
+  icon_grid='/scratch/snx3000/juckerj/WG_1_tasks/ecrad/grids/icon_grid_0008_R02B05_G.nc'
 
-varname="tsfctrad"
-call readgrid_1d(nc_name,varname,from_netcdf_2d(:,:,6),dim_len(1),dim_len(6))
+  ! icon grid
+  call get_nc_dims(icon_grid,grid_dim_name,grid_dim_len,14)
 
-varname="albvisdif"
-call readgrid_1d(nc_name,varname,from_netcdf_2d(:,:,7),dim_len(1),dim_len(6))
+  ALLOCATE(neighbor_cell_index(grid_dim_len(1),grid_dim_len(5)))
 
-varname="albnirdif"
-call readgrid_1d(nc_name,varname,from_netcdf_2d(:,:,8),dim_len(1),dim_len(6))
+  varname="neighbor_cell_index"
+  call readgrid_1d(icon_grid,varname,neighbor_cell_index(:,:),grid_dim_len(1),grid_dim_len(5))
 
-! 3D FIELDS
-varname="clc"
-call readgrid_2d(nc_name,varname,from_netcdf_3d(:,:,:,1),dim_len(1),dim_len(3),dim_len(6))
+  ! data
+  call get_nc_dims(nc_name,dim_name,dim_len,6)
 
-varname="temp"
-call readgrid_2d(nc_name,varname,from_netcdf_3d(:,:,:,2),dim_len(1),dim_len(3),dim_len(6))
+  ALLOCATE(from_netcdf_3d(dim_len(1),dim_len(3),dim_len(6),6))
+  ALLOCATE(from_netcdf_2d(dim_len(1),dim_len(6),8))
 
-varname="pres"
-call readgrid_2d(nc_name,varname,from_netcdf_3d(:,:,:,3),dim_len(1),dim_len(3),dim_len(6))
+  ! 2D FIELDS
+  varname="pres_sfc"
+  call readgrid_1d(nc_name,varname,from_netcdf_2d(:,:,1),dim_len(1),dim_len(6))
 
-varname="qc"
-call readgrid_2d(nc_name,varname,from_netcdf_3d(:,:,:,4),dim_len(1),dim_len(3),dim_len(6))
+  varname="cosmu0"
+  call readgrid_1d(nc_name,varname,from_netcdf_2d(:,:,2),dim_len(1),dim_len(6))
 
-varname="qi"
-call readgrid_2d(nc_name,varname,from_netcdf_3d(:,:,:,5),dim_len(1),dim_len(3),dim_len(6))
+  varname="qv_s"
+  call readgrid_1d(nc_name,varname,from_netcdf_2d(:,:,3),dim_len(1),dim_len(6))
 
-varname="qv"
-call readgrid_2d(nc_name,varname,from_netcdf_3d(:,:,:,6),dim_len(1),dim_len(3),dim_len(6))
+  varname="albvisdir"
+  call readgrid_1d(nc_name,varname,from_netcdf_2d(:,:,4),dim_len(1),dim_len(6))
 
-! check values from notebook
-!print*, cosmu0(x_cell_index+1,x_time_index+1)
-!print*, MAXVAL(temp(x_cell_index+1,:,x_time_index+1))
+  varname="albnirdir"
+  call readgrid_1d(nc_name,varname,from_netcdf_2d(:,:,5),dim_len(1),dim_len(6))
 
-input_2d(1,1,:) = from_netcdf_2d(x_cell_index+1,x_time_index+1,:)
-input_2d(1,2,:) = from_netcdf_2d(INT(neighbor_cell_index(x_cell_index+1,1)),x_time_index+1,:)
-input_2d(1,3,:) = from_netcdf_2d(INT(neighbor_cell_index(x_cell_index+1,2)),x_time_index+1,:)
-input_2d(1,4,:) = from_netcdf_2d(INT(neighbor_cell_index(x_cell_index+1,3)),x_time_index+1,:)
+  varname="tsfctrad"
+  call readgrid_1d(nc_name,varname,from_netcdf_2d(:,:,6),dim_len(1),dim_len(6))
 
-input_3d(1,:,1,:) = from_netcdf_3d(x_cell_index+1,:,x_time_index+1,:)
-input_3d(1,:,2,:) = from_netcdf_3d(x_cell_index+1,:,x_time_index+1,:)
-input_3d(1,:,3,:) = from_netcdf_3d(x_cell_index+1,:,x_time_index+1,:)
-input_3d(1,:,4,:) = from_netcdf_3d(x_cell_index+1,:,x_time_index+1,:)
+  varname="albvisdif"
+  call readgrid_1d(nc_name,varname,from_netcdf_2d(:,:,7),dim_len(1),dim_len(6))
 
-call infero_check(iset%initialise())
-call infero_check(iset%push_tensor(input_3d, "serving_default_input_3d"))
-call infero_check(iset%push_tensor(input_2d, "serving_default_input_2d"))
+  varname="albnirdif"
+  call readgrid_1d(nc_name,varname,from_netcdf_2d(:,:,8),dim_len(1),dim_len(6))
 
-call infero_check(oset%initialise())
-call infero_check(oset%push_tensor(pred_swflx_dn, "StatefulPartitionedCall"))
+  ! 3D FIELDS
+  varname="clc"
+  call readgrid_2d(nc_name,varname,from_netcdf_3d(:,:,:,1),dim_len(1),dim_len(3),dim_len(6))
 
-!call infero_check(iset%print())
-!call infero_check(oset%print())
+  varname="temp"
+  call readgrid_2d(nc_name,varname,from_netcdf_3d(:,:,:,2),dim_len(1),dim_len(3),dim_len(6))
 
-! Get CL arguments
-CALL get_command_argument(1, model_path)
-CALL get_command_argument(2, model_type)
+  varname="pres"
+  call readgrid_2d(nc_name,varname,from_netcdf_3d(:,:,:,3),dim_len(1),dim_len(3),dim_len(6))
 
-! 0) init infero
-call infero_check(infero_initialise())
+  varname="qc"
+  call readgrid_2d(nc_name,varname,from_netcdf_3d(:,:,:,4),dim_len(1),dim_len(3),dim_len(6))
+
+  varname="qi"
+  call readgrid_2d(nc_name,varname,from_netcdf_3d(:,:,:,5),dim_len(1),dim_len(3),dim_len(6))
+
+  varname="qv"
+  call readgrid_2d(nc_name,varname,from_netcdf_3d(:,:,:,6),dim_len(1),dim_len(3),dim_len(6))
+
+  !JJ: unclear wether push needs to be done after value assignemnt
+
+  ! input is identical for both models
+  call infero_check(iset%push_tensor(input_3d, "serving_default_input_3d"))
+  call infero_check(iset%push_tensor(input_2d, "serving_default_input_2d"))
+
+  ! LW
+  call infero_check(oset_lw%push_tensor(pred_lwflx, "StatefulPartitionedCall"))
+
+  ! SW
+  call infero_check(oset_sw%push_tensor(pred_swflx, "StatefulPartitionedCall"))
+
+  DO step=1,n_step
+    input_2d(1,1,:) = from_netcdf_2d(idx,step,:)
+    input_2d(1,2,:) = from_netcdf_2d(INT(neighbor_cell_index(idx,1)),step,:)
+    input_2d(1,3,:) = from_netcdf_2d(INT(neighbor_cell_index(idx,2)),step,:)
+    input_2d(1,4,:) = from_netcdf_2d(INT(neighbor_cell_index(idx,3)),step,:)
+
+    input_3d(1,:,1,:) = from_netcdf_3d(idx,:,step,:)
+    input_3d(1,:,2,:) = from_netcdf_3d(INT(neighbor_cell_index(idx,1)),:,step,:)
+    input_3d(1,:,3,:) = from_netcdf_3d(INT(neighbor_cell_index(idx,2)),:,step,:)
+    input_3d(1,:,4,:) = from_netcdf_3d(INT(neighbor_cell_index(idx,3)),:,step,:)
 
 
-! YAML config string
-yaml_config = "---"//NEW_LINE('A') &
-  //"  path: "//TRIM(model_path)//NEW_LINE('A') &
-  //"  type: "//TRIM(model_type)//c_null_char
+    ! apply model
+    call infero_check(model_lw%infer(iset, oset_lw ))
+    call infero_check(model_sw%infer(iset, oset_sw ))
 
-! get a infero model
-  call infero_check(model%initialise_from_yaml_string(yaml_config))
-
-! run inference
-do i=1,n_inference_reps
-  call infero_check(model%infer(iset, oset ))
-end do
+    swflx(:,:,:,step) = pred_swflx
+    lwflx(:,:,:,step) = pred_lwflx
+  ENDDO
 
 ! free the model
-  call infero_check(model%free())
+  call infero_check(model_lw%free())
+  call infero_check(model_sw%free())
 
 ! finalise infero library
   call infero_check(infero_finalise())
 
-  ! check values from notebook
-  print*, MAXVAL(pred_swflx_dn)
+
+  print*, 'Statistics:'
+  DO step=1,n_step
+    print*, 'step: ', step
+    print*, 'SW: ',MAXVAL(pred_swflx), MINVAL(pred_swflx)
+    print*, 'LW: ',MAXVAL(pred_lwflx), MINVAL(pred_lwflx)
+    print*, ''
+  ENDDO
+
 
 end program
 
-SUBROUTINE icon_grid_dims(infile,dim_name,dim_len)
+SUBROUTINE get_nc_dims(infile,dim_name,dim_len,nr_dims)
   use netcdf
 
-  INTEGER(KIND=4), INTENT(OUT) :: dim_len(14)
-  CHARACTER(LEN=50), INTENT(OUT) :: dim_name(14)
+  INTEGER(KIND=4), INTENT(OUT) :: dim_len(nr_dims)
+  CHARACTER(LEN=50), INTENT(OUT) :: dim_name(nr_dims)
   INTEGER(KIND=4) :: ncid,i
   CHARACTER(LEN=1024), INTENT(IN) :: infile
   !Open netCDF file
@@ -169,52 +205,16 @@ SUBROUTINE icon_grid_dims(infile,dim_name,dim_len)
   CALL check(nf90_open(TRIM(infile), nf90_nowrite, ncid))
   !Inquire about the dimensions
   !:-------:-------:-------:-------:-------:-------:-------:
-  !CALL check(nf90_inquire_dimension(ncid,1,dim_name(1),dim_len(1)))
-  !CALL check(nf90_inquire_dimension(ncid,2,dim_name(2),dim_len(2)))
-  !CALL check(nf90_inquire_dimension(ncid,3,dim_name(3),dim_len(3)))
-  !CALL check(nf90_inquire_dimension(ncid,4,dim_name(4),dim_len(4)))
-  !CALL check(nf90_inquire_dimension(ncid,5,dim_name(5),dim_len(5)))
-  !CALL check(nf90_inquire_dimension(ncid,6,dim_name(6),dim_len(6)))
-  !CALL check(nf90_inquire_dimension(ncid,7,dim_name(7),dim_len(7)))
-  !CALL check(nf90_inquire_dimension(ncid,8,dim_name(8),dim_len(8)))
 
-  print *, 'Dimension of NetCDF:'
-  DO i=1,14
+  print *, 'Dimension of NetCDF: ',TRIM(infile)
+  DO i=1,nr_dims
     CALL check(nf90_inquire_dimension(ncid,i,dim_name(i),dim_len(i)))
     print *, '  ',TRIM(dim_name(i)), ':', dim_len(i)
   ENDDO
   !Close netCDF file
   !:-------:-------:-------:-------:-------:-------:-------:
   CALL check(nf90_close(ncid))
-END SUBROUTINE icon_grid_dims
-
-SUBROUTINE griddims(infile,dim_name,dim_len)
-  use netcdf
-
-  INTEGER(KIND=4), INTENT(OUT) :: dim_len(6)
-  CHARACTER(LEN=50), INTENT(OUT) :: dim_name(6)
-  INTEGER(KIND=4) :: ncid,i
-  CHARACTER(LEN=1024), INTENT(IN) :: infile
-  !Open netCDF file
-  !:-------:-------:-------:-------:-------:-------:-------:
-  CALL check(nf90_open(TRIM(infile), nf90_nowrite, ncid))
-  !Inquire about the dimensions
-  !:-------:-------:-------:-------:-------:-------:-------:
-  CALL check(nf90_inquire_dimension(ncid,1,dim_name(1),dim_len(1)))
-  CALL check(nf90_inquire_dimension(ncid,2,dim_name(2),dim_len(2)))
-  CALL check(nf90_inquire_dimension(ncid,3,dim_name(3),dim_len(3)))
-  CALL check(nf90_inquire_dimension(ncid,4,dim_name(4),dim_len(4)))
-  CALL check(nf90_inquire_dimension(ncid,5,dim_name(5),dim_len(5)))
-  CALL check(nf90_inquire_dimension(ncid,6,dim_name(6),dim_len(6)))
-
-  print *, 'Dimension of NetCDF:'
-  DO i=1,6
-    print *, '  ',TRIM(dim_name(i)), ':', dim_len(i)
-  ENDDO
-  !Close netCDF file
-  !:-------:-------:-------:-------:-------:-------:-------:
-  CALL check(nf90_close(ncid))
-END SUBROUTINE griddims
+END SUBROUTINE get_nc_dims
 
 SUBROUTINE readgrid_2d(infile,varname,idata,nx,ny,nz)
   use iso_c_binding, only : c_float
