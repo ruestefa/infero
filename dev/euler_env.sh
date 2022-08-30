a992fe2fedf8530630fa16bf3c97fcaf42550579
Jonas Jucker <jonas.jucker@env.ethz.ch>
2022-08-30 17:05:05 +0200

    euler builds

---
+++ dev/euler_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -l
+
+. /cluster/apps/local/env2lmod.sh
+
+module load gcc/9.3.0
+module load openmpi/4.0.2
+module load netcdf-fortran/4.4.1
+module load cmake/3.20.3
---
+++ ecrad/euler_compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+
+. ../dev/euler_env.sh
+
+INFERO_ROOT='/cluster/scratch/juckerj/install_infero/builds/infero'
+#LIBS='-linferoapi -linferof -lnetcdff -lnetcdf'
+LIBS='-linferoapi -linferof -lnetcdff'
+FCFLAGS="-cpp -Wall -pedantic -fbacktrace -O2 -g -ffree-line-length-256 -I${NETCDF_FORTRAN_ROOT}/include -I${INFERO_ROOT}/module -Wl,-rpath,${INFERO_ROOT}/lib -L${NETCDF_FORTRAN_ROOT}/lib"
+LDFLAGS="-L${INFERO_ROOT}/lib -L${NETCDF_FORTRAN_ROOT}/lib"
+
+gfortran -o multiple_fields_model.exe $FCFLAGS $LDFLAGS $LIBS ecrad_ml.f90 
