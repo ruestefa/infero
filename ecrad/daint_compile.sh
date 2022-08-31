93b34d284a49546081aac047eee45e2c9264ea2f
Jonas Jucker <jonas.jucker@env.ethz.ch>
2022-08-31 12:27:14 +0200

    fix build-scripts

--- ecrad/daint_compile.sh
+++ ecrad/daint_compile.sh
@@ -2,7 +2,7 @@
 
 . ../dev/daint_env.sh
 
-INFERO_ROOT='/scratch/snx3000/juckerj/WG_1_tasks/ecrad/install_infero/builds/infero/'
+INFERO_ROOT='../../infero_build/builds/infero'
 LIBS='-linferoapi -linferof -lnetcdff -lnetcdf'
 FCFLAGS="-cpp -Wall -pedantic -fbacktrace -O2 -g -ffree-line-length-256 -I${NETCDF_DIR}/include -I${INFERO_ROOT}/module -Wl,-rpath,${INFERO_ROOT}/lib -L${NETCDF_DIR}/lib"
 LDFLAGS="-L${INFERO_ROOT}/lib -L${NETCDF_DIR}/lib"
--- ecrad/euler_compile.sh
+++ ecrad/euler_compile.sh
@@ -2,7 +2,7 @@
 
 . ../dev/euler_env.sh
 
-INFERO_ROOT='/cluster/scratch/juckerj/install_infero/builds/infero'
+INFERO_ROOT='../../infero_build/builds/infero'
 #LIBS='-linferoapi -linferof -lnetcdff -lnetcdf'
 LIBS='-linferoapi -linferof -lnetcdff'
 FCFLAGS="-cpp -Wall -pedantic -fbacktrace -O2 -g -ffree-line-length-256 -I${NETCDF_FORTRAN_ROOT}/include -I${INFERO_ROOT}/module -Wl,-rpath,${INFERO_ROOT}/lib -L${NETCDF_FORTRAN_ROOT}/lib"
