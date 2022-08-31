61e195b50f698dd346402cd38668b028622d0541
Jonas Jucker <jonas.jucker@env.ethz.ch>
2022-08-31 12:02:25 +0200

    pass path to models as arguments

--- ecrad/ecrad_ml.sh
+++ ecrad/ecrad_ml.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-datadir=/scratch/snx3000/juckerj/WG_1_tasks/ecrad/regress-radiation/saved_models
+datadir=$1
 eng_type=tf_c
 
 lw_model=$datadir/patch_shrNhbrWeightslvl1_all_heights_BN_mse_energyconsv1W1e-4_lw_datav2IG_SSD_MT_featNormv2/serialized_model_step_427000
