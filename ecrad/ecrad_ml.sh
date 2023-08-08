fc4bfd9c5f46f339d837d67ccd8eef27b0c59db1
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2023-08-08 17:59:03 +0200

    adapt run script interface, default model and link cudnn installation

--- ecrad/ecrad_ml.sh
+++ ecrad/ecrad_ml.sh
@@ -2,11 +2,15 @@
 
 set -eu
 
-data_dir="${1:-"/scratch/snx3000/gbertoli/icon/regress-radiation/saved_models"}"
+cudnn_path="/project/d121/ruestefa/spack/daint/cudnn-8.1.1.33-11.2/nvhpc-21.3/gofqrtudwdpdyuy23ettnsjahdrrnmub"
+export LD_LIBRARY_PATH="${cudnn_path}/lib64:$LD_LIBRARY_PATH"
 
-default_model="patch_shrNhbrWeightslvl0_all_heights_BN_mse_datav2IG_SSD_MT_featNormv2_fit_seq_MergedLwupLwdnSwupSwdn/serialized_model_steps_182400_238640_80560_188480"
-model_dir="${data_dir}/${default_model}"
-
+# Optional argument: Path to either the model parent directory, in which case
+# a default model is used, or to a specific model directory
+model_dir="${1:-"/scratch/snx3000/gbertoli/icon/regress-radiation/saved_models"}"
+# default_model="patch_shrNhbrWeightslvl0_all_heights_BN_mse_datav2IG_SSD_MT_featNormv2_fit_seq_MergedLwupLwdnSwupSwdn/serialized_model_steps_182400_238640_80560_188480"
+default_model="patch_shrNhbrWeightslvl0_all_heights_BN_mse_energyconsv4W6e-1_lwsw_datav8IG_featNormv2_fit_flatind_bs1024_unetv1/serialized_model_step_800421"
+[[ -f "${model_dir}/saved_model.pb" ]] || model_dir+="/${default_model}"
 
 eng_type=tf_c
 
