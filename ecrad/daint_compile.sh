af8add4ef24a99f33c8a2eaa0e3cb7fd240e3f91
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2023-08-08 18:23:09 +0200

    adapt compile script for daint

--- ecrad/daint_compile.sh
+++ ecrad/daint_compile.sh
@@ -2,10 +2,10 @@
 
 . ../dev/daint_env.sh
 
-INFERO_ROOT='../../infero_build/builds/infero'
+INFERO_ROOT="/project/d121/ruestefa/spack/daint/infero-0.1.2/gcc-9.3.0/5luvhyaplpwvxbn3tbxb3tpfyti4kl2l"
 LIBS='-linferoapi -linferof -lnetcdff -lnetcdf -lmpichf90'
-FCFLAGS="-cpp -Wall -pedantic -fbacktrace -O2 -g -ffree-line-length-256 -I${NETCDF_DIR}/include -I${INFERO_ROOT}/module -Wl,-rpath,${INFERO_ROOT}/lib -L${NETCDF_DIR}/lib -I${MPICH_DIR}/include"
-LDFLAGS="-L${INFERO_ROOT}/lib -L${NETCDF_DIR}/lib -L${MPICH_DIR}/lib"
+FCFLAGS="-cpp -Wall -pedantic -fbacktrace -O2 -g -ffree-line-length-256 -I${NETCDF_DIR}/include -I${INFERO_ROOT}/module -Wl,-rpath,${INFERO_ROOT}/lib64 -L${NETCDF_DIR}/lib -I${MPICH_DIR}/include -I${INFERO_ROOT}/include"
+LDFLAGS="-L${INFERO_ROOT}/lib64 -L${NETCDF_DIR}/lib -L${MPICH_DIR}/lib"
 
 ftn -o multiple_fields_model.exe $FCFLAGS $LDFLAGS $LIBS ecrad_ml.f90 
 ftn -o performance.exe $FCFLAGS $LDFLAGS $LIBS performance.f90 
