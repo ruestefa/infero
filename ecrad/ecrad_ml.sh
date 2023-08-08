8b1f4fb3c6b28cc730740ea37ee2559d4cc1411a
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2023-08-08 17:55:21 +0200

    restructure run script and add srun

--- ecrad/ecrad_ml.sh
+++ ecrad/ecrad_ml.sh
@@ -2,14 +2,15 @@
 
 set -eu
 
-datadir=$1
+data_dir="${1:-"/scratch/snx3000/gbertoli/icon/regress-radiation/saved_models"}"
+
+default_model="patch_shrNhbrWeightslvl0_all_heights_BN_mse_datav2IG_SSD_MT_featNormv2_fit_seq_MergedLwupLwdnSwupSwdn/serialized_model_steps_182400_238640_80560_188480"
+model_dir="${data_dir}/${default_model}"
+
+
 eng_type=tf_c
 
-model=$datadir/patch_shrNhbrWeightslvl0_all_heights_BN_mse_datav2IG_SSD_MT_featNormv2_fit_seq_MergedLwupLwdnSwupSwdn/serialized_model_steps_182400_238640_80560_188480
-
 # run inference
-./multiple_fields_model.exe \
-  $model \
-  $eng_type \
-
-
+cmd=(./multiple_fields_model.exe "${model_dir}" "${eng_type}")
+echo "${cmd[@]}"
+srun "${cmd[@]}"
