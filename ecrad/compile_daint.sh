61011aafcafb8131e024df654f666fb0c7d01bd3
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2023-08-09 14:26:46 +0200

    adjust compile script

--- ecrad/compile_daint.sh
+++ ecrad/compile_daint.sh
@@ -4,7 +4,9 @@
 
 INFERO_ROOT="/project/d121/ruestefa/spack/daint/infero-0.1.2/gcc-9.3.0/5luvhyaplpwvxbn3tbxb3tpfyti4kl2l"
 LIBS='-linferoapi -linferof -lnetcdff -lnetcdf -lmpichf90'
-FCFLAGS="-cpp -Wall -pedantic -fbacktrace -O2 -g -ffree-line-length-256 -I${NETCDF_DIR}/include -I${INFERO_ROOT}/module -Wl,-rpath,${INFERO_ROOT}/lib64 -L${NETCDF_DIR}/lib -I${MPICH_DIR}/include -I${INFERO_ROOT}/include"
+FCFLAGS="-cpp -Wall -pedantic -fbacktrace -ffree-line-length-256 -I${NETCDF_DIR}/include -I${INFERO_ROOT}/module -Wl,-rpath,${INFERO_ROOT}/lib64 -L${NETCDF_DIR}/lib -I${MPICH_DIR}/include -I${INFERO_ROOT}/include"
+# FCFLAGS+=" -O0 -g"
+FCFLAGS+=" -O2 -g"
 LDFLAGS="-L${INFERO_ROOT}/lib64 -L${NETCDF_DIR}/lib -L${MPICH_DIR}/lib"
 
 ftn -o multiple_fields_model.exe $FCFLAGS $LDFLAGS $LIBS ecrad_ml.f90 
