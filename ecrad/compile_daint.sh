f74748fb1eea2a212db32f1074de8ad33ab125d1
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2023-08-10 19:01:33 +0200

    exit on failure

--- ecrad/compile_daint.sh
+++ ecrad/compile_daint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-. ../dev/daint_env.sh
+. ../dev/daint_env.sh || exit
 
 INFERO_ROOT="/project/d121/ruestefa/spack/daint/infero-0.1.2/gcc-9.3.0/5luvhyaplpwvxbn3tbxb3tpfyti4kl2l"
 LIBS='-linferoapi -linferof -lnetcdff -lnetcdf -lmpichf90'
@@ -9,5 +9,5 @@ FCFLAGS="-cpp -Wall -pedantic -fbacktrace -ffree-line-length-256 -I${NETCDF_DIR}
 FCFLAGS+=" -O2 -g"
 LDFLAGS="-L${INFERO_ROOT}/lib64 -L${NETCDF_DIR}/lib -L${MPICH_DIR}/lib"
 
-ftn -o multiple_fields_model.exe $FCFLAGS $LDFLAGS $LIBS ecrad_ml.f90 
-ftn -o performance.exe $FCFLAGS $LDFLAGS $LIBS performance.f90 
+ftn -o multiple_fields_model.exe $FCFLAGS $LDFLAGS $LIBS ecrad_ml.f90 || exit
+ftn -o performance.exe $FCFLAGS $LDFLAGS $LIBS performance.f90 || exit
